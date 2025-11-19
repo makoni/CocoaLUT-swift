07c017ef912aaa040700c1625d70d782e7ef70a9
Sergey Armodin <dolph-in@yandex.ru>
2025-11-19 14:46:44 +0800

    refactor: Fix formatting in Package.swift for library definition

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CocoaLUTSwift",
-            targets: ["CocoaLUTSwift"]
+            type: .dynamic,
+            targets: ["CocoaLUTSwift"],
         ),
     ],
     targets: [
